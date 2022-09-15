25a2c0aa7d782f6e96b0b3bdbbda59ad57746df6
danisanz <daniDNZ@users.noreply.github.com>
2022-09-15 12:32:37 +0200

    Public view

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,9 @@ import bookingsRouter from './routes/bookings';
 import usersRouter from './routes/users';
 import messagesRouter from './routes/users';
 import loginRouter from './routes/login';
+import indexRouter from './routes/index';
 import passport from 'passport';
+import cookieParser from 'cookie-parser';
 require('./auth/auth');
 
 const app: Express = express();
@@ -17,13 +19,16 @@ app.set('view engine', 'jade');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/rooms', passport.authenticate('jwt', { session: false }), roomsRouter);
 app.use('/bookings', passport.authenticate('jwt', { session: false }), bookingsRouter);
 app.use('/users', passport.authenticate('jwt', { session: false }), usersRouter);
 app.use('/messages', passport.authenticate('jwt', { session: false }), messagesRouter);
 app.use('/login', loginRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req: Request, res: Response, next: NextFunction) {
