48e616af29279f1be328404988e0e1031e22ccfd
danisanz <daniDNZ@users.noreply.github.com>
2022-09-23 17:14:24 +0200

    Rooms controller connected with MySQL

--- dist/controllers/roomsController.js
+++ dist/controllers/roomsController.js
@@ -12,16 +12,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const functions_1 = require("../assets/functions");
-const rooms_json_1 = __importDefault(require("../data/rooms.json"));
-const roomsData = rooms_json_1.default;
+const mysqlConnection_1 = require("../database/mysqlConnection");
+const joi_1 = __importDefault(require("joi"));
+const roomSchema = joi_1.default.object({
+    type: joi_1.default.string().max(255).required(),
+    number: joi_1.default.string().max(255).required(),
+    price: joi_1.default.number().required(),
+    amenities: joi_1.default.array().items(joi_1.default.string()).required(),
+    description: joi_1.default.string().max(512),
+    offer: joi_1.default.boolean(),
+    discount: joi_1.default.number(),
+    cancellation: joi_1.default.string().max(512)
+});
 const roomsController = {
     index: (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
         try {
-            let rooms = yield (0, functions_1.delay)(roomsData, 500);
-            return rooms
-                ? res.json(rooms)
-                : res.status(404).json({ status: res.statusCode, message: 'Not Found' });
+            const query = `SELECT * FROM rooms;`;
+            const results = yield (0, mysqlConnection_1.dbQuery)(query);
+            if (results.length === 0) {
+                return res.status(404).json({ status: res.statusCode, message: 'Not Found' });
+            }
+            return res.json({ results });
         }
         catch (error) {
             next(error);
@@ -29,10 +40,12 @@ const roomsController = {
     }),
     show: (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
         try {
-            let room = yield (0, functions_1.delay)(roomsData.find((r) => r.id === Number(req.params.id)), 500);
-            return room
-                ? res.json(room)
-                : res.status(404).json({ status: res.statusCode, message: 'Not Found' });
+            const roomId = Number(req.params.id);
+            const results = yield (0, mysqlConnection_1.dbQuery)('SELECT * FROM rooms WHERE id = ? ;', [roomId]);
+            if (results.length === 0) {
+                return res.status(404).json({ status: res.statusCode, message: 'Not Found' });
+            }
+            return res.json({ results: results[0] });
         }
         catch (error) {
             next(error);
@@ -40,36 +53,64 @@ const roomsController = {
     }),
     store: (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
         try {
-            const data = req.body;
-            return Object.keys(data).length > 0
-                ? res.status(201).json({ status: res.statusCode, message: 'Success' })
-                : res.status(400).json({ status: res.statusCode, message: 'No body' });
+            const booking = [
+                req.body.type,
+                req.body.number,
+                req.body.price,
+                JSON.stringify(req.body.amenities),
+                req.body.description,
+                req.body.offer,
+                req.body.discount,
+                req.body.cancellation
+            ];
+            const rooms = req.body.rooms;
+            const { error } = roomSchema.validate(req.body, { abortEarly: false });
+            if (error) {
+                return res.status(400).json({ status: res.statusCode, message: error });
+            }
+            const query = 'INSERT INTO rooms (type, number, price, amenities, description, offer, discount, cancellation) VALUES (?);';
+            const results = yield (0, mysqlConnection_1.dbQuery)(query, [booking]);
+            return res.status(201).json({ status: res.statusCode, message: 'Success' });
         }
         catch (error) {
+            console.log(error);
             next(error);
         }
     }),
     update: (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
         try {
-            const data = req.body;
-            const room = yield (0, functions_1.delay)(roomsData.find((r) => r.id === Number(req.params.id)), 500);
-            if (Object.keys(data).length === 0) {
-                return res.status(400).json({ status: res.statusCode, message: 'No body' });
+            const room = req.body;
+            const roomId = Number(req.params.id);
+            const { error } = roomSchema.validate(room, { abortEarly: false });
+            if (error) {
+                return res.status(400).json({ status: res.statusCode, message: 'Bad data' });
             }
-            return room
-                ? res.sendStatus(204)
-                : res.status(404).json({ status: res.statusCode, message: 'Not Found' });
+            const query = `UPDATE rooms 
+        SET type = ?, number = ?, price = ?, amenities = ?, description = ?, offer = ?, discount = ?, cancellation = ?
+        WHERE id = ? ;`;
+            const results = yield (0, mysqlConnection_1.dbQuery)(query, [
+                room.type,
+                room.number,
+                room.price,
+                JSON.stringify(room.amenities),
+                room.description,
+                room.offer,
+                room.discount,
+                room.cancellation,
+                roomId
+            ]);
+            return res.status(201).json({ status: res.statusCode, message: 'Success' });
         }
         catch (error) {
+            console.error(error);
             next(error);
         }
     }),
     destroy: (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
         try {
-            const room = yield (0, functions_1.delay)(roomsData.find((r) => r.id === Number(req.params.id)), 500);
-            return room
-                ? res.sendStatus(204)
-                : res.status(404).json({ status: res.statusCode, message: 'Not Found' });
+            const roomId = Number(req.params.id);
+            const results = yield (0, mysqlConnection_1.dbQuery)('DELETE FROM rooms WHERE id = ?', [roomId]);
+            return res.status(204).json({ status: res.statusCode, message: 'Success' });
         }
         catch (error) {
             next(error);
--- src/controllers/roomsController.ts
+++ src/controllers/roomsController.ts
@@ -1,31 +1,40 @@
-import { delay } from '../assets/functions';
-import IRoom from '../interfaces/IRoom';
 import { Request, Response, NextFunction } from 'express';
-import roomsDataJSON from '../data/rooms.json';
+import { dbQuery } from '../database/mysqlConnection';
+import Joi from 'joi';
+import { Console } from 'console';
 
-const roomsData: any = roomsDataJSON;
+const roomSchema = Joi.object({
+  type: Joi.string().max(255).required(),
+  number: Joi.string().max(255).required(),
+  price: Joi.number().required(),
+  amenities: Joi.array().items(Joi.string()).required(),
+  description: Joi.string().max(512),
+  offer: Joi.boolean(),
+  discount: Joi.number(),
+  cancellation: Joi.string().max(512)
+});
 
 const roomsController = {
   index: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      let rooms: IRoom[] = await delay(roomsData, 500);
-      return rooms
-        ? res.json(rooms)
-        : res.status(404).json({ status: res.statusCode, message: 'Not Found' });
-
+      const query: string = `SELECT * FROM rooms;`;
+      const results: Array<any> = await dbQuery(query);
+      if (results.length === 0) {
+        return res.status(404).json({ status: res.statusCode, message: 'Not Found' });
+      }
+      return res.json({ results })
     } catch (error) {
       next(error);
     }
   },
   show: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      let room: IRoom = await delay(
-        roomsData.find((r: IRoom) => r.id === Number(req.params.id)),
-        500);
-
-      return room
-        ? res.json(room)
-        : res.status(404).json({ status: res.statusCode, message: 'Not Found' });
+      const roomId: Number = Number(req.params.id);
+      const results: Array<any> = await dbQuery('SELECT * FROM rooms WHERE id = ? ;', [roomId]);
+      if (results.length === 0) {
+        return res.status(404).json({ status: res.statusCode, message: 'Not Found' });
+      }
+      return res.json({ results: results[0] });
 
     } catch (error) {
       next(error);
@@ -33,44 +42,66 @@ const roomsController = {
   },
   store: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const data = req.body;
+      const booking = [
+        req.body.type,
+        req.body.number,
+        req.body.price,
+        JSON.stringify(req.body.amenities),
+        req.body.description,
+        req.body.offer,
+        req.body.discount,
+        req.body.cancellation
+      ];
+      const rooms = req.body.rooms
+      const { error } = roomSchema.validate(req.body, { abortEarly: false });
+      if (error) {
+        return res.status(400).json({ status: res.statusCode, message: error });
+      }
+      const query: string = 'INSERT INTO rooms (type, number, price, amenities, description, offer, discount, cancellation) VALUES (?);';
+      const results: any = await dbQuery(query, [booking]);
 
-      return Object.keys(data).length > 0
-        ? res.status(201).json({ status: res.statusCode, message: 'Success' })
-        : res.status(400).json({ status: res.statusCode, message: 'No body' })
+      return res.status(201).json({ status: res.statusCode, message: 'Success' });
     } catch (error) {
+      console.log(error)
       next(error);
     }
   },
   update: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const data = req.body
-      const room: IRoom = await delay(
-        roomsData.find((r: IRoom) => r.id === Number(req.params.id)),
-        500);
-
-      if (Object.keys(data).length === 0) {
-        return res.status(400).json({ status: res.statusCode, message: 'No body' })
+      const room = req.body;
+      const roomId: Number = Number(req.params.id);
+      const { error } = roomSchema.validate(room, { abortEarly: false });
+      if (error) {
+        return res.status(400).json({ status: res.statusCode, message: 'Bad data' });
       }
-      return room
-        ? res.sendStatus(204)
-        : res.status(404).json({ status: res.statusCode, message: 'Not Found' });
+      const query: string = `UPDATE rooms 
+        SET type = ?, number = ?, price = ?, amenities = ?, description = ?, offer = ?, discount = ?, cancellation = ?
+        WHERE id = ? ;`;
+      const results: any = await dbQuery(query,
+        [
+          room.type,
+          room.number,
+          room.price,
+          JSON.stringify(room.amenities),
+          room.description,
+          room.offer,
+          room.discount,
+          room.cancellation,
+          roomId
+        ]);
 
-
+      return res.status(201).json({ status: res.statusCode, message: 'Success' });
     } catch (error) {
+      console.error(error)
       next(error);
     }
   },
   destroy: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const room: IRoom = await delay(
-        roomsData.find((r: IRoom) => r.id === Number(req.params.id)),
-        500);
+      const roomId: Number = Number(req.params.id);
+      const results: Array<any> = await dbQuery('DELETE FROM rooms WHERE id = ?', [roomId]);
 
-      return room
-        ? res.sendStatus(204)
-        : res.status(404).json({ status: res.statusCode, message: 'Not Found' });
-
+      return res.status(204).json({ status: res.statusCode, message: 'Success' });
     } catch (error) {
       next(error);
     }
