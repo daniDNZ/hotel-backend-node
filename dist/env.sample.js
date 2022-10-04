b8a32e8dc5cb6ba73bf8b3c8b8c4437d1c892038
danisanz <daniDNZ@users.noreply.github.com>
2022-10-04 11:19:52 +0200

    env sample

---
+++ dist/env.sample.js
@@ -0,0 +1,14 @@
+"use strict";
+// FILL IN THE DATA AND RENAME THIS FILE TO env.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+// HOST: '127.0.0.1',
+// PORT: 3001,
+// ORIGINS: ['localhost:3001', 'https://hoppscotch.io'],
+// SECRET_KEY: <YOUR_SECRET_KEY>,
+// DB_USER: <YOUR_DB_USER>,
+// DB_PASSWORD: <YOUR_DB_PASSWORD>,
+// DB_NAME: <YOUR_DB_NAME>,
+// DB_URI: <YOUR_DB_URL> EXAMPLE IN ATLAS: `mongodb+srv://<DB_USER>:<DB_PASSWORD>@<YOUR_ATLAS_CLUSTER>.mongodb.net/<DB_NAME>?retryWrites=true&w=majority`
+};
+exports.default = config;
---
+++ src/env.sample.ts
@@ -0,0 +1,14 @@
+// FILL IN THE DATA AND RENAME THIS FILE TO env.ts
+
+const config = {
+  // HOST: '127.0.0.1',
+  // PORT: 3001,
+  // ORIGINS: ['localhost:3001', 'https://hoppscotch.io'],
+  // SECRET_KEY: <YOUR_SECRET_KEY>,
+  // DB_USER: <YOUR_DB_USER>,
+  // DB_PASSWORD: <YOUR_DB_PASSWORD>,
+  // DB_NAME: <YOUR_DB_NAME>,
+  // DB_URI: <YOUR_DB_URL> EXAMPLE IN ATLAS: `mongodb+srv://<DB_USER>:<DB_PASSWORD>@<YOUR_ATLAS_CLUSTER>.mongodb.net/<DB_NAME>?retryWrites=true&w=majority`
+}
+
+export default config;
